3c33aafa4e75811ad99df634176696afecdb4392
denys <dromazan@gmail.com>
2017-12-06 23:01:50 +0200

    upd

--- PowerManager.py
+++ PowerManager.py
@@ -1,9 +1,8 @@
-import tkinter as tk
 import serial
 import sys
 from guizero import *
 import glob
-from dask.compatibility import apply
+import time
 
 
 def get_serial_ports_list():
@@ -41,6 +40,8 @@ def open_port():
     if ser.is_open:
         Picture(app, image='conn.png', grid=[0, 4])
         print('port is connected')
+        #time.sleep(1)
+        request_state()
     else:
         Picture(app, image='discon.png', grid=[0, 4])
     return ser
@@ -53,79 +54,101 @@ def close_port(ser):
     else:
         Picture(app, image='discon.png', grid=[0, 4])
         print('port is disconnected')
-    pass
-
-
-def get_relay_state(relay):
-    pass
+    for i in range(0, 8):
+        Picture(app, image='discon.png', grid=[i + 2, 4])
 
 
 def send_command(relay, param):
-    pass
+    request = '{}{}'.format(relay, param)
+    print(request)
+    print(request.encode(encoding='UTF-8'))
+    ser.write(request.encode(encoding='UTF-8'))
 
 
 def switch_relay_on(relay):
-    if get_relay_state(relay) == 0:
-        send_command(relay, 'on')
-        Picture(app, image='conn.png', grid=[relay+2, 4])
+    send_command(relay, '1')
+    Picture(app, image='conn.png', grid=[relay+2, 4])
 
 
 def switch_relay_off(relay):
-    if get_relay_state(relay) == 1:
-        send_command(relay, 'off')
-        Picture(app, image='conn.png', grid=[relay+2, 4])
+    send_command(relay, '0')
+    Picture(app, image='discon.png', grid=[relay+2, 4])
 
 
-def request_data():
-    return
+def request_state():
+    request = '{}{}'.format("!", "!")
+    ser.write(request.encode(encoding='UTF-8'))
+    pins_state_str = ser.readline().decode("utf-8")
+    #print(pins_state_str)
+    #pins = list(pins_state_str)
+    print(pins_state_str)
+    for i, l in enumerate(pins_state_str):
+        if l == '0':
+            Picture(app, image='discon.png', grid=[(i + 2), 4])
+            print("printing icon discon on %s position" % (i+2))
+            i += 1
+        elif l == '1':
+            Picture(app, image='conn.png', grid=[(i + 2), 4])
+            i += 1
 
 
-app = App(title="SGRO Observatory. Setup power management",layout="grid", width=370, height=450)
+app = App(title="SGRO power management",layout="grid", width=370, height=450)
 text = Text(app, text="Select Port:", grid=[0,0])
 combo = Combo(app, options=tuple(ports), grid=[0,1])
 connect_btn = PushButton(app, text='Connect', command=open_port, grid=[0,2])
 disconnect_btn = PushButton(app, text='Disconnect', command=lambda: close_port(ser), grid=[0,3])
+#request_stat = PushButton(app, text = 'Get pins', command=lambda: request_state(), grid=[1,0])
 Picture(app, image='discon.png', grid=[0, 4])
 Text(app, text=" ", grid=[1,0])
 
+
+# for r in range(0,8):
+#     relay_label = Text(app, text="Relay %s" % (r+1), grid=[r+2,0])
+#     relay_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(r), grid=[r+2,2])
+#     relay_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(r), grid=[r+2,3])
+
+# for i in range(0,8):
+#     Picture(app, image='discon.png', grid=[i+2, 4])
+
+
 #Relay #1
 relay1_label = Text(app, text="Relay 1", grid=[2,0])
-relay1_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(1), grid=[2,2])
-relay1_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(1), grid=[2,3])
+relay1_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(0), grid=[2,2])
+relay1_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(0), grid=[2,3])
 
 #Relay #2
 relay2_label = Text(app, text="Relay 2", grid=[3,0])
-relay2_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(2), grid=[3,2])
-relay2_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(2), grid=[3,3])
+relay2_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(1), grid=[3,2])
+relay2_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(1), grid=[3,3])
 
 #Relay #1
 relay3_label = Text(app, text="Relay 3", grid=[4,0])
-relay3_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(3), grid=[4,2])
-relay3_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(3), grid=[4,3])
+relay3_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(2), grid=[4,2])
+relay3_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(2), grid=[4,3])
 
 #Relay #1
 relay4_label = Text(app, text="Relay 4", grid=[5,0])
-relay4_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(4), grid=[5,2])
-relay4_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(4), grid=[5,3])
+relay4_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(3), grid=[5,2])
+relay4_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(3), grid=[5,3])
 
 #Relay #1
 relay5_label = Text(app, text="Relay 5", grid=[6,0])
-relay5_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(5), grid=[6,2])
-relay5_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(5), grid=[6,3])
+relay5_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(4), grid=[6,2])
+relay5_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(4), grid=[6,3])
 
 #Relay #1
 relay6_label = Text(app, text="Relay 6", grid=[7,0])
-relay6_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(6), grid=[7,2])
-relay6_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(6), grid=[7,3])
+relay6_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(5), grid=[7,2])
+relay6_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(5), grid=[7,3])
 
 #Relay #1
 relay7_label = Text(app, text="Relay 7", grid=[8,0])
-relay7_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(7), grid=[8,2])
-relay7_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(7), grid=[8,3])
+relay7_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(6), grid=[8,2])
+relay7_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(6), grid=[8,3])
 
 #Relay #1
 relay8_label = Text(app, text="Relay 8", grid=[9,0])
-relay8_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(8), grid=[9,2])
-relay8_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(8), grid=[9,3])
+relay8_connect = PushButton(app, text='Connect', command=lambda: switch_relay_on(7), grid=[9,2])
+relay8_disconnect = PushButton(app, text='Disconnect', command=lambda: switch_relay_off(7), grid=[9,3])
 
 app.display()
